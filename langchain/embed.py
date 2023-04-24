eb37507fd5361f0f68b573f5b3631f8216dd8d30
Anudit Nagar <nagaranudit@gmail.com>
2023-04-24 22:11:57 +0530

    Multiprocessing Fix

--- langchain/embed.py
+++ langchain/embed.py
@@ -2,8 +2,8 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv('../.env')
-os.environ['NUMEXPR_MAX_THREADS'] = '12'
-os.environ['NUMEXPR_NUM_THREADS'] = '12'
+os.environ['NUMEXPR_MAX_THREADS'] = '32'
+os.environ['NUMEXPR_NUM_THREADS'] = '32'
 
 import logging
 logging.basicConfig(level=logging.DEBUG)
@@ -15,22 +15,28 @@ from langchain.document_loaders.csv_loader import CSVLoader
 from tqdm import tqdm
 from langchain.vectorstores import FAISS
 
-print('Loading directory')
-loader = DirectoryLoader('../census/census_csvs', glob="**/1991-B02T-*.csv", loader_cls=CSVLoader)
-print('Loading data')
-docs = loader.load()
-print('found', len(docs), 'docs')
 
-embeddings = HuggingFaceEmbeddings()
+def run_stuff():
 
-db = FAISS.from_documents(docs, embeddings)
-db.save_local("./faiss_index")
+    print('Loading directory')
+    loader = DirectoryLoader('../census/census_csvs', glob="**/1991-B02T-*.csv", loader_cls=CSVLoader)
+    print('Loading data')
+    docs = loader.load()
+    print('found', len(docs), 'docs')
 
-# deeplake_path = 'hub://anudit/test_csv'
-# dl = DeepLake(
-#     dataset_path=deeplake_path, 
-#     token=os.environ['DEEPLAKE_API_TOKEN'], 
-#     embedding_function=embeddings
-# )
+    embeddings = HuggingFaceEmbeddings()
 
-# dl.add_documents(docs)
+    db = FAISS.from_documents(docs, embeddings)
+    db.save_local("./faiss_index")
+
+    # deeplake_path = 'hub://anudit/test_csv'
+    # dl = DeepLake(
+    #     dataset_path=deeplake_path, 
+    #     token=os.environ['DEEPLAKE_API_TOKEN'], 
+    #     embedding_function=embeddings
+    # )
+
+    # dl.add_documents(docs)
+
+if __name__ == '__main__':
+    run_stuff()
